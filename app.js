3b6682cf674742134287ef55f9284b29a6d98e6f
Jonathan Cain <jcainuk@gmail.com>
2022-08-08 16:52:46 +0100

    made app.js aware of the auth routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 
+const authRoutes = require('./routes/auth.routes');
+
 const app = express();
 
 const { PORT } = process.env || 3000;
