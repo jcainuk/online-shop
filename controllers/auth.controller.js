a5d8ba0a7331d32a5857942322e46b1c17ca2ebc
Jonathan Cain <jcainuk@gmail.com>
2022-08-10 15:14:22 +0100

    created a rough shell for the signup controller function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,8 +2,10 @@ const getSignup = (req, res) => {
   res.render('customer/auth/signup');
 };
 
+const signup = (req, res) => {};
+
 const getLogin = (req, res) => {
   // ...
 };
 
-module.exports = { getSignup, getLogin };
+module.exports = { getSignup, getLogin, signup };
