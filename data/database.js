fcf1e1cc6b9a06a3d73a5606fe1cd26ece7dabd1
Jonathan Cain <jcainuk@gmail.com>
2022-08-10 15:33:31 +0100

    got the link to the mongodb database and the database name from my .env file to protect them. Connected MongoClient to the database

--- data/database.js
+++ data/database.js
@@ -1,5 +1,9 @@
 const mongodb = require('mongodb');
 
+const {
+  MONGODB_URI, DB_NAME,
+} = process.env;
+
 const { MongoClient } = mongodb;
 
-MongoClient.connect('');
+MongoClient.connect(MONGODB_URI);
