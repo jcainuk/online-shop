212c58c69727ba2539dd030a3680cdfb6e6285c8
Jonathan Cain <jcainuk@gmail.com>
2022-08-11 15:10:27 +0100

    made the signup controller function async and extracted the request body data before using the user signup method

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,8 +4,17 @@ const getSignup = (req, res) => {
   res.render('customer/auth/signup');
 };
 
-const signup = (req, res) => {
-  const user = new User();
+const signup = async (req, res) => {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city,
+  );
+
+  await user.signup();
 };
 
 const getLogin = (req, res) => {
