6b760f5510a8a1a501c5be06d60ac50417e9a7d6
Jonathan Cain <jcainuk@gmail.com>
2022-08-11 16:54:46 +0100

    start the createSessionStore function inside session.js in the config folder

--- config/session.js
+++ config/session.js
@@ -1,1 +1,9 @@
 const mongoDbStore = require('connect-mongodb-session');
+
+const createSessionStore = (session) => {
+  const MongoDBStore = mongoDbStore(session);
+
+  new MongoDBStore({
+    uri:
+  })
+};
