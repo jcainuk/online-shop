a4c8dbafb123937a5c618e6b34a84457ee2f29cb
Jonathan Cain <jcainuk@gmail.com>
2022-08-11 17:01:55 +0100

    required the environment variables into session.js and started to create a new MongoDBStore

--- config/session.js
+++ config/session.js
@@ -1,9 +1,15 @@
+const {
+  MONGODB_URI, DB_NAME, COLLECTION,
+} = process.env;
+
 const mongoDbStore = require('connect-mongodb-session');
 
 const createSessionStore = (session) => {
   const MongoDBStore = mongoDbStore(session);
 
-  new MongoDBStore({
-    uri:
-  })
+  const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    databaseName: DB_NAME,
+    collection: COLLECTION,
+  });
 };
