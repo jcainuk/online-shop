1c315e67e0a696172d0251cd08c388727a7d3225
Jonathan Cain <jcainuk@gmail.com>
2022-08-11 14:38:43 +0100

    create a constructor function inside my User class

--- models/user.model.js
+++ models/user.model.js
@@ -1,3 +1,12 @@
 class User {
-
+  constructor(email, password, fullname, street, postal, city) {
+    this.email = email;
+    this.password = password;
+    this.name = fullname;
+    this.address = {
+      street,
+      postalCode: postal,
+      city,
+    };
+  }
 }
