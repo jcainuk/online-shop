7f7515484d731f8cdadcbbea1f4880fdecea272f
Jonathan Cain <jcainuk@gmail.com>
2022-08-11 15:03:48 +0100

    start writing the signup controller function but currently lacking the correct middleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,7 +4,9 @@ const getSignup = (req, res) => {
   res.render('customer/auth/signup');
 };
 
-const signup = (req, res) => {};
+const signup = (req, res) => {
+  const user = new User();
+};
 
 const getLogin = (req, res) => {
   // ...
