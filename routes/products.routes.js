d43eb0c37a6d681f3acdc92398b658e4b4e34615
Jonathan Cain <jcainuk@gmail.com>
2022-08-15 16:49:28 +0100

    correct the path to the all-products.ejs view in products.routes.js

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/products', (req, res) => {
-  res.render('/customer/products/all-products');
+  res.render('customer/products/all-products');
 });
 
 module.exports = router;
