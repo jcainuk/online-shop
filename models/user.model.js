b1ea02dc5af194ac5529ca5632b054c35934d2fb
Jonathan Cain <jcainuk@gmail.com>
2022-08-15 18:24:39 +0100

    wrote the existsAlready method on the User class

--- models/user.model.js
+++ models/user.model.js
@@ -16,6 +16,14 @@ class User {
 
   getUserWithSameEmail = () => db.getDb().collection('users').findOne({ email: this.email });
 
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    if (existingUser) {
+      return true;
+    }
+    return false;
+  }
+
   async signup() {
     const hashedPassword = await bcrypt.hash(this.password, 12);
 
