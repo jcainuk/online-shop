4ca4d2ebca0cdc77f6f524f2de90f677062e6130
Jonathan Cain <jcainuk@gmail.com>
2022-08-15 16:11:18 +0100

    wrote the get route for '/products' in products.routes.js

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/products', (req, res) => {
+  res.render('/customer/products/all-products');
+});
+
 module.exports = router;
