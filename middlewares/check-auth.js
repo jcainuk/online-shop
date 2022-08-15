4d7d96d1d6a0a89d7acebba51ff520c45cb0c446
Jonathan Cain <jcainuk@gmail.com>
2022-08-15 16:26:50 +0100

    create the checkAuthStatus middleware function and expose it

---
+++ middlewares/check-auth.js
@@ -0,0 +1,13 @@
+const checkAuthStatus = (req, res, next) => {
+  const { uid } = req.session;
+
+  if (!uid) {
+    return next();
+  }
+
+  res.locals.uid = uid;
+  res.locals.isAuth = true;
+  next();
+};
+
+module.exports = checkAuthStatus;
