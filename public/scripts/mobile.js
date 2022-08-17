1c59f2211565e5f3f1d7de3607551331c2efcb83
Jonathan Cain <jcainuk@gmail.com>
2022-08-17 16:38:52 +0100

    create the shell of the toggleMobileMenu function and add the event listener on the mobile menu button

--- public/scripts/mobile.js
+++ public/scripts/mobile.js
@@ -1,2 +1,8 @@
 const mobileMenuBtnElement = document.getElementById('mobile-menu-btn');
 const mobileMenuElement = document.getElementById('mobile-menu');
+
+const toggleMobileMenu = () => {
+
+};
+
+mobileMenuBtnElement.addEventListener('click', toggleMobileMenu);
