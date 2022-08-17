2704a1505da95c1cd4bfa80e45fd380f2c4e85eb
Jonathan Cain <jcainuk@gmail.com>
2022-08-17 17:37:24 +0100

    set up the express and router in the admin routes and created 2 empty shell routes

---
+++ routes/admin.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/admin/products', () => {});
+
+router.get('/admin/products/new', () => {});
+
+module.exports = router;
