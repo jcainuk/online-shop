c5928c0be6ff1eed4eaf46f2c16b5dedf7729fe4
Jonathan Cain <jcainuk@gmail.com>
2022-08-17 16:41:31 +0100

    set toggleMobileMenu function to toggle a class on and off when mobile button is clicked

--- public/scripts/mobile.js
+++ public/scripts/mobile.js
@@ -2,7 +2,7 @@ const mobileMenuBtnElement = document.getElementById('mobile-menu-btn');
 const mobileMenuElement = document.getElementById('mobile-menu');
 
 const toggleMobileMenu = () => {
-
+  mobileMenuElement.classList.toggle('open');
 };
 
 mobileMenuBtnElement.addEventListener('click', toggleMobileMenu);
