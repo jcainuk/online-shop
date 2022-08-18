69d48e7c51eb1f890b0e57d22b6a8848c89f5391
Jonathan Cain <jcainuk@gmail.com>
2022-08-18 16:35:14 +0100

    create the basic constructor function for my product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,5 +1,12 @@
 class Product {
-  constructor() {}
+  constructor(productData) {
+    this.title = productData.title;
+    this.summary = productData.summary;
+    this.price = productData.price;
+    this.description = productData.description;
+    this.image = productData.image; // name of image file
+    this.imagePath = `product-data/images/${productData.image}`;
+  }
 }
 
 module.exports = Product;
