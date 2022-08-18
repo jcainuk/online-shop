a0dac597cce8b77b3599ed55ae7b64ee4f588e22
Jonathan Cain <jcainuk@gmail.com>
2022-08-18 17:19:23 +0100

    added a + sign in front of productData.price to ensure it isn't converted into a string in the Product model  & some attempted image uploads

--- models/product.model.js
+++ models/product.model.js
@@ -4,9 +4,9 @@ class Product {
   constructor(productData) {
     this.title = productData.title;
     this.summary = productData.summary;
-    this.price = productData.price;
+    this.price = +productData.price;
     this.description = productData.description;
-    this.image = productData.image; // name of image file
+    this.image = productData.image;
     this.imagePath = `product-data/images/${productData.image}`;
     this.imageUrl = `/products/assets/images/${productData.image}`;
   }
