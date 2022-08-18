4c50345f39e37d00d31a6118f68db11697a48575
Jonathan Cain <jcainuk@gmail.com>
2022-08-18 15:49:38 +0100

    require imageUploadMiddleware  inside admin routes file

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const adminController = require('../controllers/admin.controller');
+const imageUploadMiddleware = require('../middlewares/image-upload');
 
 const router = express.Router();
 
