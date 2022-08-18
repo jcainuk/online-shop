512c51bb5524fbdaa094a66be3555098a8b2884e
Jonathan Cain <jcainuk@gmail.com>
2022-08-18 15:52:20 +0100

    create the post '/products' route in admin routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -9,4 +9,6 @@ router.get('/products', adminController.getProducts);
 
 router.get('/products/new', adminController.getNewProduct);
 
+router.post('/products', imageUploadMiddleware, adminController.createNewProduct);
+
 module.exports = router;
