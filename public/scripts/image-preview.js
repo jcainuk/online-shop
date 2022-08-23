7b639c670abb1ea425e60a476fd14691e98bbb3e
Jonathan Cain <jcainuk@gmail.com>
2022-08-23 15:28:50 +0100

    create a function and a 'change' event listener to handle the image preview functionality

--- public/scripts/image-preview.js
+++ public/scripts/image-preview.js
@@ -1,2 +1,6 @@
 const imagePickerElement = document.querySelector('#image-upload-control input');
 const imagePreviewElement = document.querySelector('#image-upload-control img');
+
+const updateImagePreview = () => {};
+
+imagePickerElement.addEventListener('change', updateImagePreview);
