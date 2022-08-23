aa339f531eaa81b8cfe3f20bcef19d52136ccb41
Jonathan Cain <jcainuk@gmail.com>
2022-08-23 15:32:09 +0100

    started the updateImagePreview function

--- public/scripts/image-preview.js
+++ public/scripts/image-preview.js
@@ -1,6 +1,13 @@
 const imagePickerElement = document.querySelector('#image-upload-control input');
 const imagePreviewElement = document.querySelector('#image-upload-control img');
 
-const updateImagePreview = () => {};
+const updateImagePreview = () => {
+  const { files } = imagePickerElement;
+  if (!files || files.length === 0) {
+    imagePreviewElement.style.display = 'none';
+    return;
+  }
+  const pickedFile = files[0];
+};
 
 imagePickerElement.addEventListener('change', updateImagePreview);
