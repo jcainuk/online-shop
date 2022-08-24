7d683ddce7072e1128e1e7be5f53cb7f20430f90
Jonathan Cain <jcainuk@gmail.com>
2022-08-24 15:20:31 +0100

    required the product model in the products controller file and used the .findAll method inside getAllProducts to get a list of all the products

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,1 +1,6 @@
-const getAllProducts = () => {};
+const Product = require('../models/product.model');
+
+const getAllProducts = async (req, res, next) => {
+  const products = await Product.findAll();
+  res.render('customer/products/all-products');
+};
