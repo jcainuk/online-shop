c7c36bbd47b1eb5abc5ff7bffc7e5ed05a7ce867
Jonathan Cain <jcainuk@gmail.com>
2022-08-24 15:22:57 +0100

    add error handling to getAllProducts controller and pass the data from the database into the res.render method

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,6 +1,10 @@
 const Product = require('../models/product.model');
 
 const getAllProducts = async (req, res, next) => {
-  const products = await Product.findAll();
-  res.render('customer/products/all-products');
+  try {
+    const products = await Product.findAll();
+    res.render('customer/products/all-products', { products });
+  } catch (error) {
+    next(error);
+  }
 };
