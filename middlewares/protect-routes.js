0c6ede9f59612de79a40906fd307ab880fac3518
Jonathan Cain <jcainuk@gmail.com>
2022-08-24 15:01:37 +0100

    call next() inside the protectRoutes middleware so that the next middlleware in line can take over

--- middlewares/protect-routes.js
+++ middlewares/protect-routes.js
@@ -6,6 +6,8 @@ const protectRoutes = (req, res, next) => {
   if (req.path.startsWith('/admin') && !res.locals.isAdmin) {
     return res.redirect('/403');
   }
+
+  next();
 };
 
 module.exports = protectRoutes;
