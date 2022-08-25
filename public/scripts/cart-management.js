ff7d7514a467ee138ce613d0433b0470df2979db
Jonathan Cain <jcainuk@gmail.com>
2022-08-25 16:57:16 +0100

    started a fetch method on the addToCart function in the cart-management script

--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -1,5 +1,12 @@
 const addToCartButtonElement = document.querySelector('#product-details button');
 
-const addToCart = () => {};
+const addToCart = async () => {
+  fetch('/cart/items', {
+    method: 'POST',
+    body: JSON.stringify({
+      productId:
+    }),
+  });
+};
 
 addToCartButtonElement.addEventListener('click', addToCart);
