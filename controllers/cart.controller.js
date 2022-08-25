62ff831afaaba34ffffcb237fcfa8949f86a2bc7
Jonathan Cain <jcainuk@gmail.com>
2022-08-25 16:04:22 +0100

    add better error handling to addCartItem controller

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,8 +1,15 @@
 const Product = require('../models/product.model');
 
-const addCartItem = async (req, res) => {
-  const product = await Product.findById(req.body.productId);
-  res.locals.cart.addItem();
+const addCartItem = async (req, res, next) => {
+  let product;
+  try {
+    product = await Product.findById(req.body.productId);
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  res.locals.cart.addItem(product);
 };
 
 module.exports = {
