8f5bb77f8a7c6dc7a9fc46e3c4349134e9d2d1c6
Jonathan Cain <jcainuk@gmail.com>
2022-08-25 15:26:20 +0100

    create an addItem method on Cart class

--- models/cart.model.js
+++ models/cart.model.js
@@ -4,6 +4,21 @@ class Cart {
   }
 
   addItem(product) {
+    const cartItem = {
+      product,
+      quantity: 1,
+      totalPrice: product.price,
+    };
 
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i];
+      if (item.product.id === product.id) {
+        cartItem.quantity += 1;
+        cartItem.totalPrice += product.price;
+        this.items[i] = cartItem;
+        return;
+      }
+    }
+    this.items.push(cartItem);
   }
 }
