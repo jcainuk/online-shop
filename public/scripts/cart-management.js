883da2f0273eaba4ad7bfe8616011c13347cbcb2
Jonathan Cain <jcainuk@gmail.com>
2022-08-25 17:13:56 +0100

    add better error handling for async functions in addToCart and saved the newTotalItems key to a variable to use later

--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -2,19 +2,30 @@ const addToCartButtonElement = document.querySelector('#product-details button')
 
 const addToCart = async () => {
   const productId = addToCartButtonElement.dataset.productid;
-  const response = await fetch('/cart/items', {
-    method: 'POST',
-    body: JSON.stringify({
-      productId,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  let response;
+  try {
+    response = await fetch('/cart/items', {
+      method: 'POST',
+      body: JSON.stringify({
+        productId,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    alert('Something went wrong!');
+    return;
+  }
 
   if (!response.ok) {
     alert('Something went wrong!');
+    return;
   }
+
+  const responseData = await response.json();
+
+  const newTotalQuantity = responseData.newTotalItems;
 };
 
 addToCartButtonElement.addEventListener('click', addToCart);
