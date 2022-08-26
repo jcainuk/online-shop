a32cdc1261f77688d4042670969aeebfac6bcb3c
Jonathan Cain <jcainuk@gmail.com>
2022-08-26 16:53:21 +0100

    fixed an error in my cart model - not adding up properly

--- models/cart.model.js
+++ models/cart.model.js
@@ -15,11 +15,11 @@ class Cart {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
       if (item.product.id === product.id) {
-        cartItem.quantity += 1;
-        cartItem.totalPrice += product.price;
+        cartItem.quantity = item.quantity + 1;
+        cartItem.totalPrice = item.totalPrice + product.price;
         this.items[i] = cartItem;
 
-        this.totalQuantity += 1;
+        this.totalQuantity++;
         this.totalPrice += product.price;
         return;
       }
