1960dbad1ee43f409a55894e123e243ec4328608
Jonathan Cain <jcainuk@gmail.com>
2022-08-29 16:06:30 +0100

    add better error handling to updateCartItem function

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -9,17 +9,30 @@ const updateCartItem = async (event) => {
   const csrfToken = form.dataset.csrf;
   const quantity = form.firstElementChild.value;
 
-  const response = await fetch('/cart/items', {
-    method: 'PATCH',
-    body: JSON.stringify({
-      productId,
-      quantity,
-      _csrf: csrfToken,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  let response;
+  try {
+    response = await fetch('/cart/items', {
+      method: 'PATCH',
+      body: JSON.stringify({
+        productId,
+        quantity,
+        _csrf: csrfToken,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    alert('Something went wrong!');
+    return;
+  }
+
+  if (!response.ok) {
+    alert('Something went wrong!');
+    return;
+  }
+
+  const responseData = await response.json();
 };
 
 cartItemUpdateFormElements.forEach((formElement) => { formElement.addEventListener('submit', updateCartItem); });
