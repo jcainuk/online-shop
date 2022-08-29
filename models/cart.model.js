5ddc12048b00283e32f4c5f43b0ac6919886d1c2
Jonathan Cain <jcainuk@gmail.com>
2022-08-29 14:48:06 +0100

    refactor the updateItem method so that it can take in negative values as well as positive values and update the quantity and total price of all the items respectively

--- models/cart.model.js
+++ models/cart.model.js
@@ -32,7 +32,7 @@ class Cart {
   updateItem(productId, newQuantity) {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
-      if (item.product.id === productId) {
+      if (item.product.id === productId && newQuantity > 0) {
         const cartItem = { ...item };
         const quantityChange = newQuantity - item.quantity;
         cartItem.quantity = newQuantity;
@@ -42,6 +42,10 @@ class Cart {
         this.totalQuantity += quantityChange;
         this.totalPrice += quantityChange * product.price;
         return;
+      } if (item.product.id === productId && newQuantity <= 0) {
+        this.items.splice(i, 1);
+        this.totalQuantity -= item.quantity;
+        this.totalPrice -= item.totalPrice;
       }
     }
   }
