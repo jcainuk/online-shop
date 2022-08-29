d851f19e9aaa88a84296222485b4bf8f0807a0e9
Jonathan Cain <jcainuk@gmail.com>
2022-08-29 15:58:55 +0100

    made updateCartItem function async and performed a fetch request

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -1,6 +1,6 @@
 const cartItemUpdateFormElements = document.querySelectorAll('.cart-item-management');
 
-const updateCartItem = (event) => {
+const updateCartItem = async (event) => {
   event.preventDefault();
 
   const form = event.target;
@@ -8,6 +8,18 @@ const updateCartItem = (event) => {
   const productId = form.dataset.productid;
   const csrfToken = form.dataset.csrf;
   const quantity = form.firstElementChild.value;
+
+  const response = await fetch('/cart/items', {
+    method: 'PATCH',
+    body: JSON.stringify({
+      productId,
+      quantity,
+      _csrf: csrfToken,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 };
 
 cartItemUpdateFormElements.forEach((formElement) => { formElement.addEventListener('submit', updateCartItem); });
