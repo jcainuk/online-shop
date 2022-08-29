e91d78c8cc5b5c5c3b08cd10dc213e895a38e5ef
Jonathan Cain <jcainuk@gmail.com>
2022-08-29 15:26:08 +0100

    add an array method to the form elements to add event listener to them

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -1,1 +1,3 @@
 const cartItemUpdateFormElements = document.querySelectorAll('.cart-item-management');
+
+cartItemUpdateFormElements.forEach((formElement) => { formElement.addEventListener('submit'); });
