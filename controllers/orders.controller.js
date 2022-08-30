fb6aa71e11f3f93754c5fe462ca38d43a95733ac
Jonathan Cain <jcainuk@gmail.com>
2022-08-30 17:39:33 +0100

    add better error handling to addorder controller

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,10 +1,15 @@
 const Order = require('../models/order.model');
 const User = require('../models/user.model');
 
-const addOrder = async (req, res) => {
+const addOrder = async (req, res, next) => {
   const { cart } = res.locals;
 
-  const userDocument = await User.findById(res.locals.uid);
+  let userDocument;
+  try {
+    userDocument = await User.findById(res.locals.uid);
+  } catch (error) {
+    return next(error);
+  }
 
   const order = new Order(cart);
 };
