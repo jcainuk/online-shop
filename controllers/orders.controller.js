0ec50e1ceff5360abb2bba991163fc0f665d397b
Jonathan Cain <jcainuk@gmail.com>
2022-08-30 17:37:11 +0100

    made addOrder controller async, made it return the user by calling User's statuc findById method and passing this method the user id (uid) from res.locals

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,8 +1,10 @@
 const Order = require('../models/order.model');
 const User = require('../models/user.model');
 
-const addOrder = (req, res) => {
+const addOrder = async (req, res) => {
   const { cart } = res.locals;
+
+  const userDocument = await User.findById(res.locals.uid);
 
   const order = new Order(cart);
 };
