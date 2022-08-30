96cafb6fe66efa1930c4111a7321be446172b93a
Jonathan Cain <jcainuk@gmail.com>
2022-08-30 17:24:12 +0100

    start to create a new instance or Order in the addOrder controller...

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -2,6 +2,8 @@ const Order = require('../models/order.model');
 
 const addOrder = (req, res) => {
   const { cart } = res.locals;
+
+  const order = new Order(cart);
 };
 
 module.exports = { addOrder };
