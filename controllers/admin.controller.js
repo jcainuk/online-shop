b54cba4a0a3c4b7941b6cfd284370249dc8dfa23
Jonathan Cain <jcainuk@gmail.com>
2022-09-02 15:11:22 +0100

    update admin controllers

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,21 +1,23 @@
 const Product = require('../models/product.model');
+const Order = require('../models/order.model');
 
-const getProducts = async (req, res, next) => {
+async function getProducts(req, res, next) {
   try {
     const products = await Product.findAll();
     res.render('admin/products/all-products', { products });
   } catch (error) {
     next(error);
   }
-};
+}
 
-const getNewProduct = (req, res) => {
+function getNewProduct(req, res) {
   res.render('admin/products/new-product');
-};
+}
 
-const createNewProduct = async (req, res, next) => {
+async function createNewProduct(req, res, next) {
   const product = new Product({
-    ...req.body, image: req.file.filename,
+    ...req.body,
+    image: req.file.filename,
   });
 
   try {
@@ -26,18 +28,18 @@ const createNewProduct = async (req, res, next) => {
   }
 
   res.redirect('/admin/products');
-};
+}
 
-const getUpdateProduct = async (req, res, next) => {
+async function getUpdateProduct(req, res, next) {
   try {
     const product = await Product.findById(req.params.id);
     res.render('admin/products/update-product', { product });
   } catch (error) {
     next(error);
   }
-};
+}
 
-const updateProduct = async (req, res, next) => {
+async function updateProduct(req, res, next) {
   const product = new Product({
     ...req.body,
     _id: req.params.id,
@@ -53,10 +55,11 @@ const updateProduct = async (req, res, next) => {
     next(error);
     return;
   }
-  res.redirect('/admin/products');
-};
 
-const deleteProduct = async (req, res, next) => {
+  res.redirect('/admin/products');
+}
+
+async function deleteProduct(req, res, next) {
   let product;
   try {
     product = await Product.findById(req.params.id);
@@ -64,9 +67,45 @@ const deleteProduct = async (req, res, next) => {
   } catch (error) {
     return next(error);
   }
+
   res.json({ message: 'Deleted product!' });
-};
+}
+
+async function getOrders(req, res, next) {
+  try {
+    const orders = await Order.findAll();
+    res.render('admin/orders/admin-orders', {
+      orders,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateOrder(req, res, next) {
+  const orderId = req.params.id;
+  const { newStatus } = req.body;
+
+  try {
+    const order = await Order.findById(orderId);
+
+    order.status = newStatus;
+
+    await order.save();
+
+    res.json({ message: 'Order updated', newStatus });
+  } catch (error) {
+    next(error);
+  }
+}
 
 module.exports = {
-  getProducts, getNewProduct, createNewProduct, getUpdateProduct, updateProduct, deleteProduct,
+  getProducts,
+  getNewProduct,
+  createNewProduct,
+  getUpdateProduct,
+  updateProduct,
+  deleteProduct,
+  getOrders,
+  updateOrder,
 };
